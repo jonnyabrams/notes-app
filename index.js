0dfaeaa251d002cc0ea5804d1d9813a19ae6da83
ruiined <ruiined@gmail.com>
2022-04-04 14:33:49 +0100

    Included notes model in index.js
    Co-authored-by: Jonny Abrams <jonnyabrams@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
+const NotesModel = require('./notesModel')
+const model = new NotesModel
+model.addNote('complete this challenge by the end of today and go to a pub')
 console.log('The notes app is running');
+console.log(model.getNotes())
